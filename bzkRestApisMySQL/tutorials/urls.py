cc47a15c74393ca07405b12a6c071324d9cdc203
Shaundyl <163128967+Shaundyl@users.noreply.github.com>
2024-05-19 23:57:30 +0800

    Update urls.py

--- bzkRestApisMySQL/tutorials/urls.py
+++ bzkRestApisMySQL/tutorials/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from tutorials import views
+from .views import RegisterView, FaceLoginView, PasswordLoginView
 
 urlpatterns = [
     path('api/tutorials', views.tutorial_list),
     path('api/tutorials/<int:pk>', views.tutorial_detail),
     path('api/tutorials/published', views.tutorial_list_published),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('face-login/', FaceLoginView.as_view(), name='face-login'),
+    path('password-login/', PasswordLoginView.as_view(), name='password-login'),
 ]
